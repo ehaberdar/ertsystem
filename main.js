75d983dc0bb683a06d289d5a92a156ca9a7f84d0
Ertugrul Haberdar <118924687+ehaberdar@users.noreply.github.com>
2025-06-28 20:09:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,9 +3,15 @@ document.addEventListener("DOMContentLoaded", function () {
   // Sélection des éléments
   const toggle = document.getElementById("menu-toggle");
   const navLinks = document.getElementById("nav-links");
-
   // Clic sur le bouton burger => afficher/cacher les liens
   toggle.addEventListener("click", () => {
     navLinks.classList.toggle("show");
+  });
+
+  // fermer menu
+  document.addEventListener("click", function (e) {
+    if (!toggle.contains(e.target) && !navLinks.contains(e.target)) {
+      navLinks.classList.remove("show");
+    }
   });
 });
